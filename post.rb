5a785c97bcf7ebf8026266a949f1e9a37ea9b68a
Dr._Ob1 <drobinin575@gmail.com>
2022-04-23 15:36:13 +0300

    post class

---
+++ post.rb
@@ -0,0 +1,25 @@
+class Post
+  def initialize
+    @created_at = Time.now
+    @text = nil
+  end
+
+  def read_from_console
+
+  end
+
+  def to_strings
+
+  end
+
+  def save
+    file = File.new(file_path, "w:UTF-8")
+    to_strings.each { |string| file.puts(string) }
+    file.close
+  end
+
+  def file_path
+    file_name = @created_at.strftime("#{self.class.name}_%d-%m-%Y_%H-%M-%S.txt")
+    return "#{__dir__}/#{file_name}"
+  end
+end
